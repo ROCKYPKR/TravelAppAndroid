f6146b9fe1ee2b840f6947b50d2cdb1734a27d13
pfieffer <cbpmgr@gmail.com>
2019-05-04 21:30:42 +0545

    Create activities for package detail and hotel details

---
+++ app/src/main/java/com/eshna/travelapp/activity/HotelDetailActivity.java
@@ -0,0 +1,15 @@
+package com.eshna.travelapp.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.eshna.travelapp.R;
+
+public class HotelDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hotel_detail);
+    }
+}
---
+++ app/src/main/java/com/eshna/travelapp/activity/PackageDetailActivity.java
@@ -0,0 +1,15 @@
+package com.eshna.travelapp.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.eshna.travelapp.R;
+
+public class PackageDetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_package_detail);
+    }
+}
